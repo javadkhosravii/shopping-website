f42abd11ee0527c955cd4a71f1e11dde663c3ed0
javad <javadkhosravi777@gmail.com>
2024-04-26 19:18:08 +0200

    Refactor Product component in Product.js

--- src/pages/shop/Product.js
+++ src/pages/shop/Product.js
@@ -9,16 +9,18 @@ const Product = (props) => {
   return (
     <div className="border flex rounded-md w-64 h-96 m-4 p-2">
       <div className="flex flex-col place-items-center gap-4">
-        <img alt="pic" src={productImage} className="hover:scale-105 duration-300"/>
+        <img
+          alt="pic"
+          src={productImage}
+          className="hover:scale-105 duration-300"
+        />
         <h5 className="text-center font-bold"> {productName}</h5>
         <p>price : {price}€</p>
         <div className="flex place-items-center gap-2">
           <button className="btn btn-info btn-sm" onClick={() => addToCart(id)}>
             +
           </button>
-          <span className="">
-            {cartItems?.filter((row) => row.id === id)[0]?.count}
-          </span>
+          <span>{cartItems?.filter((row) => row.id === id)[0]?.count}</span>
           {isInCart && (
             <button
               className="btn btn-info btn-sm"
