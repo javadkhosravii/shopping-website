84a1d010706ed9e8305488011904448d9a6aa617
javad <javadkhosravi777@gmail.com>
2024-06-04 12:54:58 +0200

    webshop changes

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,35 +1,48 @@
-import React from "react";
-// import Axios from "axios";
-
-export async function getStaticProps() {
-  const response = await fetch(
-    "https://learnwebcode.github.io/json-example/posts.json"
-  );
-  const data = await response.json();
-  return {
-    props: {
-      posts: data.posts,
-    },
-  };
-}
-// Axios.get("https://learnwebcode.github.io/json-example/posts.json").then(
-//   (res) => {
-//     console.log(res.data);
-//   }
-// );
-
-const Blog = (props) => {
+const Blogdata = [
+  {
+    id: 1,
+    title: "Our Products",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam voluptatum odit officia nesciunt recusandae rem cumque provident possimus, vero nobis fugit sint! Debitis omnis hic laboriosam ullam consectetur aliquam sequi a itaque, ab est odio iure asperiores sit cumque esse neque reiciendis consequuntur praesentium? Iste corrupti repudiandae ut expedita suscipit?",
+  },
+  {
+    id: 2,
+    title: "Our Services",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam voluptatum odit officia nesciunt recusandae rem cumque provident possimus, vero nobis fugit sint! Debitis omnis hic laboriosam ullam consectetur aliquam sequi a itaque, ab est odio iure asperiores sit cumque esse neque reiciendis consequuntur praesentium? Iste corrupti repudiandae ut expedita suscipit?",
+  },
+  {
+    id: 3,
+    title: "Our Team",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam voluptatum odit officia nesciunt recusandae rem cumque provident possimus, vero nobis fugit sint! Debitis omnis hic laboriosam ullam consectetur aliquam sequi a itaque, ab est odio iure asperiores sit cumque esse neque reiciendis consequuntur praesentium? Iste corrupti repudiandae ut expedita suscipit?",
+  },
+  {
+    id: 4,
+    title: "Our Vision",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam voluptatum odit officia nesciunt recusandae rem cumque provident possimus, vero nobis fugit sint! Debitis omnis hic laboriosam ullam consectetur aliquam sequi a itaque, ab est odio iure asperiores sit cumque esse neque reiciendis consequuntur praesentium? Iste corrupti repudiandae ut expedita suscipit?",
+  },
+  {
+    id: 5,
+    title: "Our Mission",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam voluptatum odit officia nesciunt recusandae rem cumque provident possimus, vero nobis fugit sint! Debitis omnis hic laboriosam ullam consectetur aliquam sequi a itaque, ab est odio iure asperiores sit cumque esse neque reiciendis consequuntur praesentium? Iste corrupti repudiandae ut expedita suscipit?",
+  },
+];
+const Blog = () => {
   return (
-    <section className="flex w-full h-full">
-      <h1>Welcome to our blog page</h1>
-      <div>
-        {props.posts.map((post, index) => {
+    <section className="container mx-auto flex flex-col gap-6 w-full h-full bg-[#3d2a2a37]">
+      <h1 className="text-4xl font-bold p-4 text-[#37518b] slg:text-center smd:text-3xl">
+        Welcome to our blog page
+      </h1>
+      <div className="flex flex-col gap-4 p-4">
+        {Blogdata.map((post, index) => {
           return (
-            <div key={index}>
-              <h2>
-                <link rel="stylesheet" href="">{post.title}</link>
-              </h2>
-              <p>{post.content}</p>
+            <div className="flex flex-col gap-2" key={index}>
+              <h2 className="text-2xl font-medium">{post.title}</h2>
+              <p className="px-4">{post.content}</p>
+              <hr />
             </div>
           );
         })}
